0747b59002d712514aad427b67dd0f11429af69d
roro123323 <130724642+roro123323@users.noreply.github.com>
2023-07-17 18:28:30 -0500

    add python file

---
+++ PyBank/main.py
@@ -0,0 +1,9 @@
+'Author: Roel De Los Santos'
+'title: PyBank'
+'Date : 7/16/2023'
+
+import pandas as pd
+import os
+print(os.getcwd())
+
+'df = read_csv('
---
+++ Pypoll/main.py
@@ -0,0 +1,3 @@
+'Author: Roel De Los Santos'
+'title: PyPoll'
+'Date : 7/16/2023'
